3dc56be343931bbc42dbb78ac748549869f4baa6
chen.yusong <bykifs@163.com>
2017-11-21 10:25:44 +0800

    2

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import io.swagger.annotations.ApiOperation;
  * Created by cys on 2017/8/22.
  */
 @RestController
+@RefreshScope
 @RequestMapping(value = "/api/demo")
 public class HelloController {
 
