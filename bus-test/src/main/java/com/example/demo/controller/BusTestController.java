86b9d8b2ba471540fad5bbe5412da5b5e8ee65d6
chen.yusong <bykifs@163.com>
2017-11-21 10:17:03 +0800

    Auto stash before merge of "master" and "origin/master"

--- bus-test/src/main/java/com/example/demo/controller/BusTestController.java
+++ bus-test/src/main/java/com/example/demo/controller/BusTestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.FeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import java.util.Map;
  */
 
 @RestController
+@RefreshScope
 @RequestMapping(value = "/bus-test")
 public class BusTestController {
 
